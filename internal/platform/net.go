de3741eb14f70dcab5862777068c6740bc406d5a
dmksnnk <kasyanenkodg@gmail.com>
2025-07-02 23:50:37 +0200

    refactor(net): remove unused LocalListener and simplify DialUDP

--- internal/platform/net.go
+++ internal/platform/net.go
@@ -9,30 +9,17 @@ import (
 // MTU is a safe maximum size of a UDP packet.
 const MTU = 1200
 
-type LocalListener struct {
-	ListenConfig net.ListenConfig
-}
-
-func (l LocalListener) ListenUDP(ctx context.Context, port int) (net.PacketConn, error) {
-	self := net.UDPAddr{
-		IP:   net.IPv4(127, 0, 0, 1),
-		Port: port,
-	}
-
-	return l.ListenConfig.ListenPacket(ctx, "udp", self.String())
-}
-
 type LocalDialer struct {
 	Dialer net.Dialer
 }
 
 func (d *LocalDialer) DialUDP(ctx context.Context, port int) (net.Conn, error) {
-	gameServer := net.UDPAddr{
+	localhost := net.UDPAddr{
 		IP:   net.IPv4(127, 0, 0, 1),
 		Port: port,
 	}
 
-	conn, err := d.Dialer.DialContext(ctx, "udp", gameServer.String())
+	conn, err := d.Dialer.DialContext(ctx, "udp", localhost.String())
 	if err != nil {
 		return nil, fmt.Errorf("dial UDP: %w", err)
 	}
